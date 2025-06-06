a06c4b2b69b73a1c7495532f0f536377d25ce1bf
delgado <alex.o.delgado@gmail.com>
2025-06-06 02:57:30 -0400

    address more sql issues

--- migrations/20250605231308_create_tables.up.sql
+++ migrations/20250605231308_create_tables.up.sql
@@ -46,8 +46,8 @@ CREATE TABLE node (
 -- Edge Details Table
 CREATE TABLE edge (
   dag_id UUID NOT NULL,
-  source_id UUID NOT NULL,
-  dest_id UUID NOT NULL,
+  source_id TEXT NOT NULL,
+  dest_id TEXT NOT NULL,
   PRIMARY KEY(dag_id, source_id, dest_id),
   FOREIGN KEY(dag_id) REFERENCES dag(dag_id),
   FOREIGN KEY(dag_id, source_id) REFERENCES node(dag_id, node_id),
