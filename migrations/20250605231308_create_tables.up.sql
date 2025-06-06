8e3589f0fd18d37b88ff2b6bf1cf10f27a03c6b0
delgado <alex.o.delgado@gmail.com>
2025-06-06 01:27:04 -0400

    Refine table schema

--- migrations/20250605231308_create_tables.up.sql
+++ migrations/20250605231308_create_tables.up.sql
@@ -2,8 +2,7 @@
 
 -- Plan DAG Table
 CREATE TABLE dag (
-  dataset_id UUID PRIMARY KEY,
-  plan_dag JSONB NOT NULL,
+  id UUID PRIMARY KEY,
   paused BOOL NOT NULL,
   modified_by TEXT NOT NULL,
   modified_date TIMESTAMPTZ NOT NULL
@@ -22,22 +21,42 @@ CREATE TYPE state_type AS ENUM (
   'running', -- compute reports job starting
   'success', -- job succeed
   'failed', -- job failed
-  'removed' -- node is not part of the plan dag
+  'disabled' -- node is not part of the plan dag
 );
 
 -- Node Details Table
 CREATE TABLE node (
-  dataset_id UUID NOT NULL,
-  node_id TEXT NOT NULL,
+  id UUID PRIMARY KEY,
+  dag_id UUID NOT NULL,
   compute compute_type NOT NULL,
   data_product TEXT NOT NULL,
   version TEXT NOT NULL,
   eager BOOL NOT NULL,
   passthrough JSONB,
   state state_type NOT NULL,
+  run_id TEXT,
+  run_link TEXT,
   passback JSONB,
   modified_by TEXT NOT NULL,
   modified_date TIMESTAMPTZ NOT NULL,
-  PRIMARY KEY (dataset_id, node_id),
-  FOREIGN KEY(dataset_id) REFERENCES dag(dataset_id)
+  UNIQUE(id, dag_id),
+  FOREIGN KEY(dag_id) REFERENCES dag(id)
 );
+
+-- Add index so we can get all the nodes for a dag quickly
+CREATE INDEX node_dag_id ON node(dag_id);
+
+-- Edge Details Table
+CREATE TABLE edge (
+  id UUID PRIMARY KEY,
+  dag_id UUID NOT NULL,
+  source_id UUID NOT NULL,
+  dest_id UUID NOT NULL,
+  UNIQUE(source_id, dest_id),
+  FOREIGN KEY(dag_id) REFERENCES dag(id),
+  FOREIGN KEY(source_id, dag_id) REFERENCES node(id, dag_id),
+  FOREIGN KEY(dest_id, dag_id) REFERENCES node(id, dag_id)
+);
+
+-- Add index so we can get all the edges for a dag quickly
+CREATE INDEX edge_dag_id ON edge(dag_id);
